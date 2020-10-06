c1717070e8b1186cc1b7887bafcc30594dcef7f6
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-06 16:25:56 +0700

    2. The Products Overview Screen

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
+import productsReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  products: productsReducer
+})
+
+const store = createStore(rootReducer);
+
 
 export default function App() {
   return (
-    <View>
-      <Text>A new app!</Text>
-    </View>
+    <Provider store = {store}>
+      <View>...</View>
+    </Provider>
   );
 }
