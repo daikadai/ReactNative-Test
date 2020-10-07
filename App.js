3fc317b110c402b7510e2901d5b5a02ae6bf2290
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-07 12:44:40 +0700

    7. Using Custom Fonts

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import ShopNavigator from './navigation/ShopNavigator';
-import productsReducer from './store/reducers/products';
+import React, { useState } from "react";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
+import ShopNavigator from "./navigation/ShopNavigator";
+import productsReducer from "./store/reducers/products";
+import { AppLoading } from "expo";
+import { useFonts } from 'expo-font';
 
 const rootReducer = combineReducers({
-  products: productsReducer
-})
+  products: productsReducer,
+});
 
 const store = createStore(rootReducer);
 
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+  });
+  if (!fontsLoaded) {
+    return (
+      <AppLoading/>
+    );
+  }
+
   return (
-    <Provider store = {store}>
+    <Provider store={store}>
       <ShopNavigator />
     </Provider>
   );
